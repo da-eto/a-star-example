2e0fefd95fd669ff2aa2cd685bfc4a8fb6d522c1
Ilya Pleshakov <da-eto@ya.ru>
2014-04-13 13:32:40 +0400

    creatures movement animation

--- javascripts/rabbit.js
+++ javascripts/rabbit.js
@@ -1,26 +1,38 @@
 jQuery(document).ready(function ($) {
-    var $board = $('.game-board');
 
-    $board.find('div').draggable({
-        containment: ".game-board",
-        revert: 'invalid'
+    var $board = $('.game-board');
+    var $characters = $board.find('[data-role="character"]');
+    var $cells = $board.find('td');
+    var positioningDuration = 250;
+
+    $characters.draggable({
+        containment: $board,
+        zIndex: 100,
+        revert: 'invalid',
+        revertDuration: positioningDuration
     });
 
-    $board.find('td').droppable({
-        accept: function() {
-            return ($(this).has('div').length == 0);
+    $cells.droppable({
+        accept: function (element) {
+            return $(this).children().length == 0 && $(element).data('role') == 'character';
         },
         drop: function (event, ui) {
             var $character = $(ui.draggable);
-            $character.detach();
+            var $oldParent = $character.parent();
             var $target = $(this);
 
-            $target.append($character);
             $target.removeClass('drag-over');
-            $character.css({
-                left: 0,
-                top: 0
-            })
+            $character
+                .detach()
+                .appendTo($target)
+                .css({
+                    left: parseInt($character.css('left'), 10) + $oldParent.offset().left - $target.offset().left,
+                    top: parseInt($character.css('top'), 10) + $oldParent.offset().top - $target.offset().top
+                })
+                .animate({
+                    left: 0,
+                    top: 0
+                }, positioningDuration);
         },
         over: function () {
             $(this).addClass('drag-over');
